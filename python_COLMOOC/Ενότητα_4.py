de60068e0f8cd7c5daca6c07c38755ad3870c792
mariatri18 <mariatri13@gmail.com>
2020-05-14 20:23:04 +0300

    end

--- python_COLMOOC/Ενότητα_4.py
+++ python_COLMOOC/Ενότητα_4.py
@@ -2,10 +2,16 @@ import statistics
 
 list = []
 
-
-def square(sm):
+def square(l,m,s):
+    for i in range(s):
+        sq = m - l[i]
+        ans = sq**2
+        
+        yield ans
     
-    yield
+    
+
+           
 
 print('Δώσε όσους αριθμούς θες! Για να σταματήσεις δώσε κάποιον χαρακτήρα')
 
@@ -15,7 +21,6 @@ try:
     while True: 
         my_list.append(int(input())) 
           
-# if input is not-integer, just print the list 
 except: 
     print(my_list)
 
@@ -29,7 +34,8 @@ s = len(my_list)
 
 for i in range (0, s):
     sq = m - my_list[i]
-    ans = sq**2
-    print('Tο τετράγωνα της διαφοράς κάθε τιμής από τον μέσο όρο', ans)
+    ans1 = sq**2
+    print('Tο τετράγωνα της διαφοράς κάθε τιμής από τον μέσο όρο είναι', ans1)
+
 
 print('Μέγεθος της λίστας', s)
