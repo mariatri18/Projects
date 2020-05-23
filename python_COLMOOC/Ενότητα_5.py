feab0e39e7f182991e22470a9612d2dc58bbd0e3
mariatri18 <mariatri13@gmail.com>
2020-05-23 13:15:15 +0300

    finish

--- python_COLMOOC/Ενότητα_5.py
+++ python_COLMOOC/Ενότητα_5.py
@@ -1,2 +1,22 @@
-with open('python_COLMOOC/myfile.txt', 'w', encoding='utf-8') as f:
-    f.write('ελα')
+import statistics
+
+with open('python_COLMOOC/inputdata.txt', 'r', encoding='utf-8') as fin:
+    content = fin.read().splitlines()
+
+numlist=[]
+for i in content:
+    numbers = i.split(',')
+    for n in numbers:
+        numlist.append(float(n))
+
+
+print(numlist)
+m = statistics.mean(numlist)
+
+ta = statistics.stdev(numlist)
+
+with open('python_COLMOOC/outputdata.txt', 'w', encoding='utf-8') as f:
+    f.write('Μέσος όρος = ')
+    f.write(str(m) + '\n')
+    f.write('Τυπική απόκλιση = ')
+    f.write(str(ta))
