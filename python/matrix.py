f282e60a5045e6c9fc0f4f3d90dd55cc79d11271
mariatri18 <mariatri13@gmail.com>
2020-05-25 23:29:08 +0300

    second print

--- python/matrix.py
+++ python/matrix.py
@@ -21,8 +21,9 @@ with open('python/outfile.csv', 'w' ) as out:
             
             for row in a[s]: 
                 for l in range(0, s):
-                
-                    df = pd.DataFrame({'x1': [a[0]],'x2': [a[1]],'x3': [a[2]],'x4': [a[3]],'x5': [a[4]],'x6': [a[5]],'x7': [a[6]],'x8': [a[7]]})
-                    df.to_csv('python/outfile.csv')
+                    df1 = pd.DataFrame({'x1': [a[0]],'x2': [a[1]],'x3': [a[2]],'x4': [a[3]],'x5': [a[4]],'x6': [a[5]],'x7': [a[6]],'x8': [a[7]]})
+                    df2 = pd.DataFrame({'x1': [a[0]],'x2': [a[1]],'x3': [a[2]],'x4': [a[3]],'x5': [a[4]],'x6': [a[5]],'x7': [a[6]],'x8': [a[7]]})
+                    df2.to_csv('python/outfile.csv')
+                    df2.to_csv('python/outfile.csv')
                 # out.write(str(a))
                 # out.write(str("\n\n"))
