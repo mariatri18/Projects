e8b40b02427d11eb033eaba2bc336734bdba5c21
mariatri18 <mariatri13@gmail.com>
2020-05-25 18:00:19 +0300

    outfile

--- python/matrix.py
+++ python/matrix.py
@@ -2,14 +2,17 @@ import numpy as np
 from os import system, name
 
 def clear(): 
-    if name == 'nt': 
+    if name == 's': 
         _ = system('cls') 
 clear()
 
-s = 5
+s = 62
 i=0
-
-for i in range (0, s):
-    print(i+1) 
-    A = np.random.randint (2, size=(8,8))
-    print(A,"\n")
+with open('python/outfile.txt', 'w') as out:
+    for i in range (0, s):
+            print(i+1) 
+            A = np.random.randint (2, size=(8,8))
+            print(A,"\n")
+            out.write(str(A))
+            out.write(str("\n\n"))
+            
---
+++ python/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+
+def clear(): 
+    if name == 'nt': 
+        _ = system('cls') 
+clear()
