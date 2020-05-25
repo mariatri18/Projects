50b9ce017896cf9bbd3e5f2ded9adbcc1380e48c
mariatri18 <mariatri13@gmail.com>
2020-05-25 17:29:00 +0300

    clean

--- python/matrix.py
+++ python/matrix.py
@@ -1,5 +1,10 @@
 import numpy as np
+from os import system, name
 
+def clear(): 
+    if name == 'nt': 
+        _ = system('cls') 
+clear()
 
 s = 5
 i=0
@@ -8,4 +13,3 @@ for i in range (0, s):
     print(i+1) 
     A = np.random.randint (2, size=(8,8))
     print(A,"\n")
-       
