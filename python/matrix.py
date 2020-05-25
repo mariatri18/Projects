176ae91a8d0bdf13c45dc8b9a7e6b866acb5ba5c
mariatri18 <mariatri13@gmail.com>
2020-05-25 17:20:50 +0300

    sudoku

--- python/matrix.py
+++ python/matrix.py
@@ -1,5 +1,11 @@
 import numpy as np
 
-A = np.random.randint (2, size=(8,8))
 
-print(A)
+s = 5
+i=0
+
+for i in range (0, s):
+    print(i+1) 
+    A = np.random.randint (2, size=(8,8))
+    print(A,"\n")
+       
