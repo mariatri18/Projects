bbd151d6c6f450c7cf1f14a016405d2208776d11
mariatri18 <mariatri13@gmail.com>
2020-05-27 11:11:56 +0300

    .

--- python/matrix.py
+++ python/matrix.py
@@ -1,29 +1,21 @@
 import numpy as np
 from os import system, name
 import pandas as pd
-
-def clear(): 
-    if name == 's': 
-        _ = system('cls') 
-clear()
+import csv
 
 s = 2
-l = 0
-xl = 0
 
-with open('python/outfile.csv', 'w' ) as out:
+with open('python/outfile.csv','w') as out:
+    #out_reader = csv.writer(out)
     
     for l in range (0, s):
             
-            print(l+1) 
             a = np.random.randint (2, size=(8,8))
+            alist = list(a)
             print(a,"\n")
+            print(alist)
             
-            for row in a[s]: 
-                for l in range(0, s):
-                    df1 = pd.DataFrame({'x1': [a[0]],'x2': [a[1]],'x3': [a[2]],'x4': [a[3]],'x5': [a[4]],'x6': [a[5]],'x7': [a[6]],'x8': [a[7]]})
-                    df2 = pd.DataFrame({'x1': [a[0]],'x2': [a[1]],'x3': [a[2]],'x4': [a[3]],'x5': [a[4]],'x6': [a[5]],'x7': [a[6]],'x8': [a[7]]})
-                    df2.to_csv('python/outfile.csv')
-                    df2.to_csv('python/outfile.csv')
-                # out.write(str(a))
-                # out.write(str("\n\n"))
+               
+            df = pd.DataFrame([alist[0]]+[alist[1]]+[alist[2]]+[alist[3]]+[alist[4]]+[alist[5]]+[alist[6]]+[alist[7]])
+
+            df.to_csv("python/outfile.csv", line_terminator='\n', mode='a')
