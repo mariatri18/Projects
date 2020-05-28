8533a3d7a22c9e489e1a198144408f3f3c7ed487
mariatri18 <mariatri13@gmail.com>
2020-05-28 18:39:06 +0300

    finish

--- python/nQueens.py
+++ python/nQueens.py
@@ -2,12 +2,9 @@ import subprocess
 
 class QueenChessBoard:
     def __init__(self, size):
-        # board has dimensions size x size
+        
         self.size = size
-        # columns[r] is a number c if a queen is placed at row r and column c.
-        # columns[r] is out of range if no queen is place in row r.
-        # Thus after all queens are placed, they will be at positions
-        # (columns[0], 0), (columns[1], 1), ... (columns[size - 1], size - 1)
+        
         self.columns = []
  
     def get_size(self):
@@ -23,20 +20,20 @@ class QueenChessBoard:
         return self.columns.pop()
  
     def is_this_column_safe_in_next_row(self, column):
-        # index of next row
+        
         row = len(self.columns)
  
-        # check column
+        
         for queen_column in self.columns:
             if column == queen_column:
                 return False
  
-        # check diagonal
+        
         for queen_row, queen_column in enumerate(self.columns):
             if queen_column - queen_row == column - row:
                 return False
  
-        # check other diagonal
+      
         for queen_row, queen_column in enumerate(self.columns):
             if ((self.size - queen_column) - queen_row
                 == (self.size - column) - row):
@@ -55,26 +52,23 @@ class QueenChessBoard:
  
  
 def print_all_solutions_to_n_queen(size):
-    """Display a chessboard for each possible configuration of placing n queens
-    on an n x n chessboard where n = size and print the number of such
-    configurations."""
+    
     board = QueenChessBoard(size)
     number_of_solutions = print_all_solutions_helper(board)
-    print('Number of solutions:', number_of_solutions)
+    print('Arithmos Lysewn:', number_of_solutions)
  
 def print_all_solutions_helper(board):
-    """Display a chessboard for each possible configuration of filling the given
-    board with queens and return the number of such configurations."""
+    
     size = board.get_size()
  
-    # if board is full, display solution
+    
     if size == board.get_queens_count():
         board.display()
         print()
         return 1
  
     number_of_solutions = 0
-    # place queen in next row
+    
     for column in range(size):
         if board.is_this_column_safe_in_next_row(column):
             board.place_in_next_row(column)
@@ -84,9 +78,5 @@ def print_all_solutions_helper(board):
     return number_of_solutions
  
  
-n = int(input('Enter n: \n'))
+n = int(input('Dwse n: \n'))
 print_all_solutions_to_n_queen(n)
-
-
-# with open("out.txt", "w+") as output:
-#     subprocess.call(["python", "./nQueens.py"], stdout=output);
