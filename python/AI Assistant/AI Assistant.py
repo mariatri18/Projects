244cb8dee797eb89293e67d68ca390d6a4c2fcea
mariatri18 <mariatri13@gmail.com>
2020-08-20 10:17:51 +0300

    Add files via upload

--- python/AI Assistant/AI Assistant.py
+++ python/AI Assistant/AI Assistant.py
@@ -2,5 +2,11 @@ import pyttsx3 #pip install pyttsx3
 import datetime
 
 engine = pyttsx3.init()
-engine.say("Good Morning")
-engine.runAndWait()
+# engine.say("Good Morning")
+# engine.runAndWait()
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()         #function text to speech
+
+speak("Good Morning")
