4f3e43bfad0ecb362853786f6cfaa07eb4a66e3d
mariatri18 <mariatri13@gmail.com>
2020-08-20 10:18:26 +0300

    updated

--- python/AI Assistant/AI Assistant.py
+++ python/AI Assistant/AI Assistant.py
@@ -9,4 +9,19 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()         #function text to speech
 
+def time():
+    Time = datetime.datetime.now().strftime("%I:%M:%S")  #Take the current time
+    speak(Time)
+
+def date():
+    year = int(datetime.datetime.now().year)
+    month = int(datetime.datetime.now().month)
+    date = int(datetime.datetime.now().day)
+    speak("The current date is")
+    speak(date)
+    speak(month)
+    speak(year)
+
 speak("Good Morning")
+time()
+date()
