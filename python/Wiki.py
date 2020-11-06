31c571fdc970f68fbe039ea2544cfb5dfe230709
mariatri18 <mariatri13@gmail.com>
2020-11-06 23:24:12 +0200

    Wiki search

---
+++ python/Wiki.py
@@ -0,0 +1,30 @@
+import requests
+import string
+from bs4 import BeautifulSoup
+
+Enter_input = input("Search: ")
+u_i = string.capwords(Enter_input)
+lists = u_i.split()
+word = "_".join(lists)
+
+url = "https://en.wikipedia.org/wiki/"+word
+
+def wikibot(url):
+    url_open = requests.get(url)
+    soup = BeautifulSoup(url_open.content,'html.parser')
+    details = soup('table',{'class':'infobox'})
+    for i in details:
+        h = i.find_all('tr')
+        for j in h:
+            heading = j.find_all('th')
+            detail = j.find_all('td')
+            if heading is not None and  detail is not None:
+                for x,y in zip(heading,detail):
+                    print("{} :: {}".format(x.text,y.text))
+                    print("------------------")
+    for i in range(1,3):
+        print(soup('p')[i].text)
+wikibot(url)     
+
+
+print(url)
