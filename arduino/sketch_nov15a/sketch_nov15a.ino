61a3a2997d904dc364e54e0434e0ce7038f2824f
mariatri18 <mariatri13@gmail.com>
2020-11-16 12:57:55 +0200

    arduino

---
+++ arduino/sketch_nov15a/sketch_nov15a.ino
@@ -0,0 +1,14 @@
+int LED_PIN = 12;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_PIN, LOW);
+  delay(500);
+}
--- python/Stem/Chapter 2/kef2_24.py
+++ python/Stem/Chapter 2/kef2_24.py
@@ -3,6 +3,7 @@ import turtle
 fred = turtle.Pen()
 fred.shape("turtle")
 
+
 fred.forward(100)
 fred.left(120)
 fred.forward(100)
